bd87d9d42232c48647fe9b6bf18f22ede2339189
Kévin Gomez <geek63@gmail.com>
2013-09-04 13:08:54 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,67 @@
+FakerServiceProvider
+====================
+
+A [Faker](https://github.com/fzaninotto/Faker) service provider for [Silex](http://silex.sensiolabs.org/).
+
+**N.B**: this provider is *locale aware*. It will automatically configure Faker
+to use the most suited locale for the request.
+
+
+## Install
+
+Install Silex using [Composer](http://getcomposer.org/).
+
+Install the FakerServiceProvider adding `kphoen/faker-service-provider` to your composer.json or from CLI:
+
+```
+$ php composer.phar require kphoen/faker-service-provider:dev-master
+```
+
+
+## Usage
+
+Initialize it using `register`. Its default behavior is to use Faker's factory
+and try to guess the right locale to use.
+```php
+<?php
+
+use KPhoen\Provider\FakerServiceProvider;
+
+$app->register(new FakerServiceProvider());
+```
+
+In this example, we use a custom factory and force the locale (both in the
+provider and in the whole application) to *fr_FR*:
+```php
+<?php
+
+use KPhoen\Provider\FakerServiceProvider;
+
+$app->register(new FakerServiceProvider('\Acme\Faker\Factory', $guessLocale = false), array(
+    'locale' => 'fr_FR',
+));
+```
+
+From PHP
+```php
+<?php
+
+$app->get('/hello', function() use ($app) {
+    return 'Hello ' . $app['faker']->name;
+});
+```
+
+From [Twig](http://twig.sensiolabs.org/)
+```html
+<!DOCTYPE html>
+<html>
+    <body>
+        <p>Hello {{ app.faker.name }}!</p>
+    </body>
+</html>
+```
+
+
+## Licence
+
+This provider is released under the MIT license.
---
+++ src/KPhoen/Provider/FakerServiceProvider.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace KPhoen\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * A Faker service provider for Silex.
+ *
+ * @author Kévin Gomez <contact@kevingomez.fr>
+ */
+class FakerServiceProvider implements ServiceProviderInterface
+{
+    protected $factoryClass, $guessLocale;
+
+    /**
+     * Add a 'faker' service initialized with the language the most adapted to
+     * the request.
+     *
+     * @param string    $factoryClass   The factory class used to create Faker's
+     *                                  generator instance.
+     * @param bool      $guessLocale    Indicates if the locale should be guessed
+     *                                  from the request or read from the
+     *                                  application settings.
+     */
+    public function __construct($factoryClass = '\Faker\Factory', $guessLocale = true)
+    {
+        $this->factoryClass = $factoryClass;
+        $this->guessLocale = $guessLocale;
+    }
+
+    public function register(Application $app)
+    {
+    }
+
+    /**
+     * Add a 'faker' service initialized with the language the most adapted to
+     * the request.
+     *
+     * @param Application $app The current application.
+     */
+    public function boot(Application $app)
+    {
+        $app['faker'] = $app->share(function($app) {
+            if ($this->guessLocale) {
+                $language = $this->getBestLanguage($app['request'], $app['locale']);
+            } else {
+                $language = $app['locale'];
+            }
+
+            $factoryClass = $this->factoryClass;
+            return $factoryClass::create($language);
+        });
+    }
+
+    /**
+     * Finds the best language for a given request.
+     *
+     * @param Request $request  The request.
+     * @param string  $default  The default language if nothing satisfying is
+     *                          found in the request.
+     */
+    protected function getBestLanguage(Request $request, $default = null)
+    {
+        foreach ($request->getLanguages() as $language) {
+            if (strpos($language, '_') !== false) {
+                return $language;
+            }
+        }
+
+        return $default;
+    }
+}
